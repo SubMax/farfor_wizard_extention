c6834494071e10def9c7ee863b87a09b98b3b7b6
Subb Maxx <subbbbbbbbbject@gmail.com>
2021-03-19 14:36:54 +0500

    Раскрывающийся список

--- popup.js
+++ popup.js
@@ -1,9 +1,6 @@
 getStartedBtn = document.getElementById('get_started');
 getStartedBtn.addEventListener('click', getStarted);
 
-arrowBack = document.getElementById('arrow_back');
-arrowBack.addEventListener('click', getBack);
-
 function getStarted() {
     console.log("get start");
     mainCard = document.getElementById('main_card');
@@ -12,10 +9,23 @@ function getStarted() {
     listCard.hidden = 0;
 }
 
+arrowBack = document.getElementById('arrow_back');
+arrowBack.addEventListener('click', getBack);
 function getBack() {
     console.log('back');
     mainCard = document.getElementById('main_card');
     listCard = document.getElementById('list_card');
     mainCard.hidden = 0;
     listCard.hidden = 1;
+}
+
+orders = document.getElementById('orders');
+orders.addEventListener('click', getOrders);
+function getOrders() {
+    console.log('Orders');
+    listOrders = document.getElementsByClassName('list_orders');
+    for (let el of listOrders) {
+        el.hidden = !el.hidden
+    }
+    console.log('listOrders.hidden');
 }
